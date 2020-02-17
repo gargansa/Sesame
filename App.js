f0ad9e7e296434e1035181a87d70284d3d3671eb
gargansa <gargansa@hotmail.com>
2020-02-17 11:16:32 -0700

    categories

--- App.js
+++ App.js
@@ -1,13 +1,126 @@
 import React, { Component } from 'react';
-import Nav from './Nav/Nav.js';
 import './App.css';
+import { Header, ThemeProvider, ListItem } from 'react-native-elements';
+import TouchableScale from 'react-native-touchable-scale';
+// import LinearGradient from 'react-native-linear-gradient'; 
+
+
+
 class App extends Component {
   state = {//initialize state
-    title: "Sesame"
+    title: "Sesame",
+
+    list: [
+      {
+        name: 'A',
+        quantity: '5',
+      },
+      {
+        name: 'B',
+        quantity: '1',
+      },
+      {
+        name: 'C',
+        quantity: '1',
+      },
+      {
+        name: 'D',
+        quantity: '3',
+      },
+      {
+        name: 'E',
+        quantity: '1',
+      },
+      {
+        name: 'F',
+        quantity: '1',
+      },
+      {
+        name: 'G',
+        quantity: '2',
+      },
+      {
+        name: 'H',
+        quantity: '1',
+      },
+      {
+        name: 'I',
+        quantity: '1',
+      },
+      {
+        name: 'J',
+        quantity: '1',
+      },
+      {
+        name: 'K',
+        quantity: '1',
+      },
+      {
+        name: 'L',
+        quantity: '1',
+      },
+      {
+        name: 'M',
+        quantity: '1',
+      },
+      {
+        name: 'N',
+        quantity: '1',
+      },
+      {
+        name: 'O',
+        quantity: '1',
+      },
+      {
+        name: 'P',
+        quantity: '1',
+      },
+      {
+        name: 'Q',
+        quantity: '1',
+      },
+      {
+        name: 'R',
+        quantity: '1',
+      },
+      {
+        name: 'S',
+        quantity: '1',
+      },
+      {
+        name: 'T',
+        quantity: '1',
+      },
+      {
+        name: 'U',
+        quantity: '1',
+      },
+      {
+        name: 'V',
+        quantity: '1',
+      },
+      {
+        name: 'W',
+        quantity: '1',
+      },
+      {
+        name: 'X',
+        quantity: '1',
+      },
+      {
+        name: 'Y',
+        quantity: '1',
+      },
+      {
+        name: 'Z',
+        quantity: '1',
+      },
+    ]
   }
   constructor() {
     super()
     console.log('Constructor Called')
+
     //this.state.color.s = this.randomColor()//chooses the random color happens before the component is displayed
   }
 
@@ -31,8 +144,26 @@ class App extends Component {
     console.log('render Called')
 
     return (
-      <div className="center">
-        <Nav title={this.state.title} />
+      <div className="container">
+        <div className="center">
+          <Header
+            leftComponent={{ icon: 'menu', color: '#fff' }}
+            centerComponent={{ text: 'Sesame', style: { color: '#fff' } }}
+            rightComponent={{ icon: 'home', color: '#fff' }}
+          />
+
+          {
+            this.state.list.map((l, i) => (
+              <ListItem
+                key={i}
+                title={l.name}
+                badge={{ value: l.quantity, textStyle: { color: 'orange' } }}
+                chevron
+                bottomDivider
+              />
+            ))
+          }
+        </div>
       </div>
     );
 
